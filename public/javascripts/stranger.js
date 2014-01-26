ad3178cc24741aa8ad20094e580529ae3e4b110e
valentinpascau <pascauvalentin@gmail.com>
2014-01-26 16:56:47 +0200

    add styling for stranger page

--- public/javascripts/stranger.js
+++ public/javascripts/stranger.js
@@ -1,3 +1,8 @@
 $(document).ready(function($) {
-  
+  $("a[href='#signup']").on('click', function(){
+    $('.slideForms').toggleClass('formTransition');
+  });
+  $("a[href='#signin']").on('click', function(){
+    $('.slideForms').toggleClass('formTransition');
+  });
 });
