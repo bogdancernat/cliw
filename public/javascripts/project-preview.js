924c902c044000f684a0601f3523ca67d7a5e2d8
valentinpascau <pascauvalentin@gmail.com>
2014-02-10 00:01:02 +0200

    full-screen mode

--- public/javascripts/project-preview.js
+++ public/javascripts/project-preview.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
   var $frame = $('#effects');
   var $wrap  = $frame.parent();
+  var flag = false;
   var option = {
       horizontal: 1,
       itemNav: 'forceCentered',
@@ -28,15 +29,19 @@ $(document).ready(function(){
 
   var sly = new Sly($frame, option).init();
 
+   var temWi = $('.list').width();
+   $('.list').width(temWi + 1);
+
+
     $('.toStart').on('click', function () {
       sly.toStart();
     });
-    $('.toLeft').on('click', function () {
-      sly.prev();
-    });
-    $('.toRight').on('click', function () {
-      sly.next();
-    });
+    // $('.toLeft').on('click', function () {
+    //   sly.prev();
+    // });
+    // $('.toRight').on('click', function () {
+    //   sly.next();
+    // });
 
     $('.toEnd').on('click', function () {
       sly.toEnd();
@@ -57,7 +62,7 @@ $(document).ready(function(){
         , b = 500
         , ratio = b/a
         ;
-      sly.reload();
+      
       var w_w = window.innerWidth
         , w = w_w - 80
         , h = w * ratio
@@ -67,12 +72,90 @@ $(document).ready(function(){
           'width': w + 'px',
           'height': h +'px'
         });
-        }else if(w_w > 850){
+        }else if(w_w > 850 && !flag){
           $('.prj-prev').css({ 
           'width': '800px',
           'height': '500px'
         });
         }
+        sly.reload();
+        var temWi = $('.list').width();
+        $('.list').width(temWi + 1);
     })
+
+    function cancelFullScreen(el) {
+      var requestMethod = el.cancelFullScreen||el.webkitCancelFullScreen||el.mozCancelFullScreen||el.exitFullscreen;
+      if (requestMethod) { // cancel full screen.
+          requestMethod.call(el);
+      } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+          var wscript = new ActiveXObject("WScript.Shell");
+          if (wscript !== null) {
+              wscript.SendKeys("{F11}");
+          }
+      }
+    }
+
+        function requestFullScreen(el) {
+          // alert(2)
+            // Supports most browsers and their versions.
+            var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullScreen;
+
+            if (requestMethod) { // Native full screen.
+                requestMethod.call(el);
+            } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+                var wscript = new ActiveXObject("WScript.Shell");
+                if (wscript !== null) {
+                    wscript.SendKeys("{F11}");
+                }
+            }
+            return false
+        }
+
+        function toggleFull() {
+            var elem = document.body; // Make the body go full screen.
+            var isInFullScreen = (document.fullScreenElement && document.fullScreenElement !== null) ||  (document.mozFullScreen || document.webkitIsFullScreen);
+
+            if (isInFullScreen) {
+                cancelFullScreen(document);
+            } else {
+                requestFullScreen(elem);
+            }
+            return false;
+        }
+
+  $(document).on('click', '.toFullScreen', function () {
+    if(!flag) {
+      toggleFull();
+      $(this).find('.glyphicon').removeClass('glyphicon-fullscreen');
+      $(this).find('.glyphicon').addClass('glyphicon-resize-small');
+      $('.prj-prev').css({
+        'width': '900px',
+        'height': '600px'
+      });
+      $('.frame').css({
+        'height': '85%'
+      })
+      sly.reload();
+      var temWi = $('.list').width();
+      $('.list').width(temWi + 1);
+      flag = true;
+    }else {
+      toggleFull();
+      $(this).find('.glyphicon').removeClass('glyphicon-resize-small');
+      $(this).find('.glyphicon').addClass('glyphicon-fullscreen');
+      $('.prj-prev').css({
+        'width': '800px',
+        'height': '500px'
+      });
+      $('.frame').css({
+        'height': '75%'
+      })
+      flag = false;
+      sly.reload();
+      var temWi = $('.list').width();
+      $('.list').width(temWi + 1);
+
+    }
+  });
 
 });
